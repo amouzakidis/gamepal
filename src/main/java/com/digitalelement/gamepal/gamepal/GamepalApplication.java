da4600d6b1a1018e27152deb9862d00fc686b7cd
Alexandros Mouzakidis <alex.mouzakidis@booking.com>
2019-11-14 11:44:15 +0100

    the beginning is half of everything :)

---
+++ src/main/java/com/digitalelement/gamepal/gamepal/GamepalApplication.java
@@ -0,0 +1,13 @@
+package com.digitalelement.gamepal.gamepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalelement/gamepal/gamepal/GamepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalelement.gamepal.gamepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
